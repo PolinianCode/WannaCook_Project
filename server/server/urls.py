d76889586ba076f8cd562fcb14a1b85a3e7af15d
Danylo Koval <37926017+PolinianCode@users.noreply.github.com>
2023-12-19 22:14:04 +0100

    Test comment page

--- server/server/urls.py
+++ server/server/urls.py
@@ -5,10 +5,21 @@ urlpatterns = [
     path('api/users/', views.api_get_users, name='api_get_users'),
     path('api/user/create/', views.api_add_user, name='api_add_user'),
     path('api/user/delete/<str:username>/', views.api_remove_user, name='api_remove_user'),
+
+
+
     path('api/recipe/add/', views.api_add_recipe, name='api_add_recipe'),
     path('api/recipe/delete/<int:recipe_id>/', views.api_delete_recipe, name="api_delete_recipe"),
     path('api/recipe/modify/<int:recipe_id>/', views.api_modify_recipe, name="api_modify_recipe"),
+
+
+
+
     path('api/categories/', views.api_load_recipe_categories, name='api_load_recipe_categories'),
+
+
+
+    
     path('api/recipe/favorites/', views.api_add_favorite, name='api_add_favorite'),
     path('api/comment/add/', views.api_add_comment, name='api_add_comment'),
     path('api/comment/delete/<int:comment_id>/', views.api_remove_comment, name='api_remove_comment'),
