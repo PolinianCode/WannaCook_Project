d0616ecf2d0045ed00bb099e5d099349add5b605
Danylo Koval <37926017+PolinianCode@users.noreply.github.com>
2023-12-20 16:49:29 +0100

    login user and add/remove recipe from favorites

--- server/app/views.py
+++ server/app/views.py
@@ -1,14 +1,11 @@
-from django.http import JsonResponse
 from rest_framework.response import Response
 from django.views.decorators.csrf import csrf_exempt
-from django.core.serializers import serialize
 from django.utils import timezone
 from rest_framework.decorators import api_view
 from .models import Recipes, Users, Categories, Favorites, Comments
-from .serializers import UsersSerializer, CommentsSerializer, CategoriesSerializer
-import json
+from .serializers import UsersSerializer, CommentsSerializer, CategoriesSerializer, RecipesSerializer, FavoritesSerializer
 from rest_framework import status
-from .utils import check_user_exist, check_user_email_exist, check_user_favorites_exists
+from .utils import is_user_exists, is_email_taken, is_favorite_exists
 from django.shortcuts import get_object_or_404
 
 # --------------------- USER ---------------------
@@ -29,30 +26,36 @@ def api_get_users(request):
 @csrf_exempt
 def api_add_user(request):
     try:
-        nickname_data = request.data.get('nickname', '')
+        username_data = request.data.get('nickname', '')
         email_data = request.data.get('email', '')
-        password_data = request.data.get('password', '')
-        is_moderator_data = request.data.get('is_moderator', '0')
+
+        if not username_data:
+            return Response({'error': 'Missing username parameter!'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        if is_user_exists(username_data):
+            return Response({'error': 'User with this username already exists!'}, status=status.HTTP_400_BAD_REQUEST)
+        
+        if is_email_taken(email_data):
+            return Response({'error': 'Email is already taken!'}, status=status.HTTP_400_BAD_REQUEST)
+
 
         current_datetime = timezone.now()
         formatted_datetime = current_datetime.strftime("%Y-%m-%d")
+        
 
-        if not nickname_data or not email_data or not password_data:
-            return Response({'error': 'Missing parameters!'}, status=status.HTTP_400_BAD_REQUEST)
-
-        if check_user_exist(nickname_data) or check_user_email_exist(email_data):
-            return Response({'error': 'User exists!'}, status=status.HTTP_400_BAD_REQUEST)
-        else:
-            user = Users(nickname=nickname_data, email=email_data, password=password_data, registration_date=formatted_datetime, is_moderator=is_moderator_data)
-            user.save()
-
+        serializer = UsersSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.validated_data['registration_date'] = formatted_datetime
+        if serializer.is_valid():
+            serializer.save()
             return Response({'message': 'User has been created!'}, status=status.HTTP_201_CREATED)
+        return Response({'error': 'Invalid data provided'}, status=status.HTTP_400_BAD_REQUEST)
     except Exception as e:
         print(e)
         return Response({'error': 'Internal Server Error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
     
 
-@api_view(['POST'])
+@api_view(['GET'])
 @csrf_exempt
 def api_get_user(request):
     username = request.data.get('username')
@@ -61,7 +64,7 @@ def api_get_user(request):
     if not username or not password:
         return Response({"error": "Invalid username or password"}, status=status.HTTP_400_BAD_REQUEST)
     
-    user = Users.objects.filter(username=username, password=password)
+    user = Users.objects.filter(nickname=username, password=password).first()
 
 
     if user is not None:
@@ -72,8 +75,11 @@ def api_get_user(request):
 # Remove a user from the database by nickname.
 @api_view(['DELETE'])
 @csrf_exempt
-def api_remove_user(request, username):
+def api_remove_user(request):
     try:
+
+        username = request.data.get('username')
+
         user = get_object_or_404(Users, nickname=username)
         user.delete()
         return Response({"message": f"User {username} has been deleted."})
@@ -85,84 +91,38 @@ def api_remove_user(request, username):
 # --------------------- RECIPE ---------------------
 # This section contains API endpoints related to recipe operations.
 
-
-# Add a new recipe to the database.
+# Add recipe
 @api_view(['POST'])
 @csrf_exempt
 def api_add_recipe(request):
     try:
-        if request.method == "POST":
-            data = json.loads(request.body)
-            title_data = data.get('title', '')
-            description_data = data.get('description', '')
-            instructions_data = data.get('instruction', '')
-            user_id_data = data.get('user_id', None)
-            category_name_data = data.get('category_id', '')
 
-            current_datetime = timezone.now()
-            formatted_datetime = current_datetime.strftime("%Y-%m-%d")
+        current_datetime = timezone.now()
+        formatted_datetime = current_datetime.strftime("%Y-%m-%d")
 
-            if user_id_data is None:
-                return Response({"message": f"Missing user id"}, status=status.HTTP_400_BAD_REQUEST)
-
-            if not title_data or not description_data or not instructions_data or not category_name_data:
-                return Response({"message": f"Missing required parameters"}, status=status.HTTP_400_BAD_REQUEST)
-            
-            user = Users.objects.get(pk=user_id_data)
-            category = Categories.objects.get(pk=category_name_data)
-        
-            recipe = Recipes(
-                title=title_data,
-                description=description_data,
-                instruction=instructions_data,
-                created_date=formatted_datetime,
-                rating_sum=0,
-                rating_num=0,
-                category=category,
-                user=user
-            )
-            recipe.save()
-
-            return Response({"message": f"The recipe has been added successfully"}, status=status.HTTP_201_CREATED)
-
-        
-        else:
-            return Response({'error': 'Request Error'}, status=status.HTTP_400_BAD_REQUEST)
-
+        serializer = RecipesSerializer(data=request.data)
+        serializer.is_valid(raise_exception=True)
+        serializer.validated_data['created_date'] = formatted_datetime
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "The recipe has been added successfully"}, status=status.HTTP_201_CREATED)
+        return Response({"message": "Invalid data provided"}, status=status.HTTP_400_BAD_REQUEST)
     except Exception as e:
         print(e)
-        return Response({"message": f"Recipe cant be added to DB {e}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+        return Response({"message": f"Recipe couldn't be added: {e}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
-# Modify an existing recipe in the database.
+# Modify Recipe
 @csrf_exempt
 @api_view(['PUT'])
 def api_modify_recipe(request, recipe_id):
-
     try:
         recipe = get_object_or_404(Recipes, pk=recipe_id)
+        serializer = RecipesSerializer(recipe, data=request.data, partial=True)
 
-        if request.method == "PUT":
-            data = json.loads(request.body)
-            title_data = data.get('title', recipe.title)
-            description_data = data.get('description', recipe.description)
-            instructions_data = data.get('instruction', recipe.instruction)
-            category_name_data = data.get('category_id', recipe.category.id)
-
-            
-            recipe.title = title_data
-            recipe.description = description_data
-            recipe.instruction = instructions_data
-
-           
-            category = get_object_or_404(Categories, pk=category_name_data)
-            recipe.category = category
-
-            recipe.save()
-
-            return Response({"message": f"The recipe has been modified successfully"}, status=status.HTTP_200_OK)
-        else:
-            return Response({'error': 'Invalid Request Method'}, status=status.HTTP_400_BAD_REQUEST)
-
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "The recipe has been modified successfully"}, status=status.HTTP_200_OK)
+        return Response({"error": "Invalid data provided"}, status=status.HTTP_400_BAD_REQUEST)
     except Exception as e:
         print(e)
         return Response({"message": f"Recipe modification failed: {e}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
@@ -170,13 +130,16 @@ def api_modify_recipe(request, recipe_id):
 # Delete a recipe from the database.
 @csrf_exempt
 @api_view(['DELETE'])
-def api_delete_recipe(request, recipe_id):
+def api_delete_recipe(request):
      try:
+
+        recipe_id = request.data.get('recipe_id')
+
         recipe = get_object_or_404(Recipes, recipe_id=recipe_id)
 
         recipe.delete()
 
-        return Response({"message": f"Recipe {recipe_id} has been deleted."}, status=status.HTTP_204_NO_CONTENT)
+        return Response({"message": f"Recipe {recipe_id} has been deleted."}, status=status.HTTP_200_OK)
 
      except Exception as e:
         print(e)
@@ -196,32 +159,20 @@ def api_load_recipe_categories(request):
     serialized_categories = CategoriesSerializer(all_categories, many=True)
     return Response({'Categories': serialized_categories.data}, status=status.HTTP_200_OK)
 
-# Add recipe to favorites 
+# Add recipe to favorites
 @api_view(['POST'])
 @csrf_exempt
 def api_add_favorite(request):
     try:
-        if request.method == 'POST':
-            data = request.data
-            recipe_id_data = data.get('recipe_id', None)
-            user_id_data = data.get('user_id', None)
-
-            if not recipe_id_data or not user_id_data:
-                return Response({"message": "Missing requirement parameters"}, status=status.HTTP_400_BAD_REQUEST)
-
-            if check_user_favorites_exists(recipe_id_data, user_id_data):
-                return Response({"message": "Recipe is already in the favorites list for this user"}, status=status.HTTP_200_OK)
-
-            else:
-                favorite = Favorites(recipe_id=recipe_id_data, user_id=user_id_data)
-                favorite.save()
-
-                return Response({"message": f"Recipe with id {recipe_id_data} added to favorites list of user with id {user_id_data}"}, status=status.HTTP_201_CREATED)
-        else:
-            return Response({'error': 'Request Error'}, status=status.HTTP_400_BAD_REQUEST)
+        serializer = FavoritesSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response({"message": "Recipe added to favorites successfully"}, status=status.HTTP_201_CREATED)
+        return Response({"message": "Invalid data provided"}, status=status.HTTP_400_BAD_REQUEST)
     except Exception as e:
         print(e)
         return Response({'error': 'Internal Server Error'}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
 
 # Remove recipe from favorites
 @api_view(['POST'])
@@ -246,36 +197,23 @@ def api_remove_favorite(request):
         print(e)
         return Response({"message": f"Favorite for user {user_id} and recipe {recipe_id} has been deleted."}, status=status.HTTP_404_NOT_FOUND)
 
-@csrf_exempt
+# Add comment to recipe
 @api_view(['POST'])
+@csrf_exempt
 def api_add_comment(request):
     try:
-        if request.method == 'POST':
-            data = request.data
-            recipe_id_data = data.get('recipe_id', None)
-            user_id_data = data.get('user_id', None)
-            comment_text_data = data.get('comment_text', '')
-
-            current_datetime = timezone.now()
-            formatted_datetime = current_datetime.strftime("%Y-%m-%d")
-
-            if not recipe_id_data or not user_id_data or not comment_text_data:
-                return Response({"message": "Missing required parameters"}, status=status.HTTP_400_BAD_REQUEST)
-
-            # Check if the recipe and user exist before adding the comment
-            recipe = get_object_or_404(Recipes, pk=recipe_id_data)
-            user = get_object_or_404(Users, pk=user_id_data)
-
-            comment = Comments(recipe=recipe, user=user, comment_text=comment_text_data, comment_date=formatted_datetime)
-            comment.save()
-
+        serializer = CommentsSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
             return Response({"message": "Comment added successfully"}, status=status.HTTP_201_CREATED)
-
-        return Response({'error': 'Request Error'}, status=status.HTTP_400_BAD_REQUEST)
+        return Response({"message": "Invalid data provided"}, status=status.HTTP_400_BAD_REQUEST)
     except Exception as e:
         print(e)
         return Response({"message": f"Comment couldn't be added: {e}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
 
+
+
+# Remove comment from recipe
 @api_view(['DELETE'])
 @csrf_exempt
 def api_remove_comment(request, comment_id):
@@ -288,9 +226,28 @@ def api_remove_comment(request, comment_id):
     except Exception as e:
         print(e)
         return Response({"message": f"Comment {comment_id} doesn't exist"}, status=status.HTTP_404_NOT_FOUND)
-    
 
 
+# Edit comment 
+@api_view(['PUT'])
+@csrf_exempt
+def api_modify_comment(request, comment_id):
+    try:
+        comment = get_object_or_404(Comments, pk=comment_id)
+
+        data = request.data
+        comment_text_data = data.get('comment_text', comment.comment_text)
+
+        comment.comment_text = comment_text_data
+        comment.save()
+
+        return Response({"message": f"Comment {comment_id} has been modified successfully"}, status=status.HTTP_200_OK)
+    except Exception as e:
+        print(e)
+        return Response({"message": f"Comment modification failed: {e}"}, status=status.HTTP_500_INTERNAL_SERVER_ERROR)
+
+
+# Get comments for recipe
 @api_view(['GET'])
 @csrf_exempt
 def api_get_comments(request, recipe_id):
--- server/server/urls.py
+++ server/server/urls.py
@@ -4,13 +4,14 @@ from app import views
 urlpatterns = [
     path('api/users/', views.api_get_users, name='api_get_users'),
     path('api/user/create/', views.api_add_user, name='api_add_user'),
-    path('api/user/delete/<str:username>/', views.api_remove_user, name='api_remove_user'),
+    path('api/user/get/', views.api_get_user, name='api_get_user'),
+    path('api/user/delete/', views.api_remove_user, name='api_remove_user'),
 
 
 
     path('api/recipe/add/', views.api_add_recipe, name='api_add_recipe'),
-    path('api/recipe/delete/<int:recipe_id>/', views.api_delete_recipe, name="api_delete_recipe"),
-    path('api/recipe/modify/<int:recipe_id>/', views.api_modify_recipe, name="api_modify_recipe"),
+    path('api/recipe/delete/', views.api_delete_recipe, name="api_delete_recipe"),
+    path('api/recipe/modify/', views.api_modify_recipe, name="api_modify_recipe"),
 
 
 
