576f672ed5acc935e7f8563c8c03603f9041dcc2
Danylo Koval <37926017+PolinianCode@users.noreply.github.com>
2024-01-08 00:47:33 +0100

    user urls

---
+++ backend/users/urls.py
@@ -0,0 +1,9 @@
+
+from django.urls import path, include
+from . import views
+
+urlpatterns = [
+    path('register/', views.register_user, name="register_user"),
+    path('login/', views.login_user, name="login_user"),
+    path('logout/', views.logout_user, name="logout_user")
+]
