e57f7122e0f90df11916ea52927f6cf912a3c910
vizels <mirchuk.georgiy@gmail.com>
2024-01-08 01:49:01 +0100

    Added router and viewset for category

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -1,8 +1,22 @@
-
 from django.urls import path, include
 from django.contrib import admin
+from rest_framework.routers import DefaultRouter
+
+from categories.views import CategoriesViewSet
+
+router = DefaultRouter()
+router.register(r'categories', CategoriesViewSet, basename='categories')
+
 
 urlpatterns = [
-    path('api/user/', include('users.urls')),
+    path('api/', include(router.urls)),
+
+    # path('api/comments/', include('comments.urls')),
+    # path('api/favorites/', include('favorites.urls')),
+    # path('api/ingredients/', include('ingredients.urls')),
+    # path('api/ratings/', include('ratings.urls')),
+    # path('api/recipes/', include('recipes.urls')),
+    # path('api/user/', include('users.urls')),
+
     path('admin/', admin.site.urls)
 ]
--- backend/categories/views.py
+++ backend/categories/views.py
@@ -1,3 +1,8 @@
-from django.shortcuts import render
+from rest_framework import viewsets
+from .models import Categories
+from .serializers import CategoriesSerializer
 
 # Create your views here.
+class CategoriesViewSet(viewsets.ModelViewSet):
+    queryset = Categories.objects.all()
+    serializer_class = CategoriesSerializer
